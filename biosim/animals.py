94f75b61fdeefb1e7b8354cb538fad5b420e7a32
Peter Langdalen <pelangda@nmbu.no>
2020-06-04 21:55:51 +0200

    Added function for the weight at birth

--- biosim/animals.py
+++ biosim/animals.py
@@ -62,6 +62,15 @@ class Animal:
     def migrate(self):
         pass
 
+    @classmethod
+    def birth_weight(cls, p):
+        """
+
+        :param p:
+        :return:
+        """
+        return np.random.normal(p['w_birth'], p['sigma_birth'])
+
 
 class Herbivore(Animal):
     """
@@ -84,6 +93,7 @@ class Carnivore(Animal):
     """
     Carnivore subclass
     """
+
     def __init__(self, age=0, weight=None):
         """
         Carnivore subclass constructor
@@ -92,6 +102,6 @@ class Carnivore(Animal):
 
     parameters = {'w_birth': 6.0, 'sigma_birth': 1.0, 'beta': 0.75,
                   'eta': 0.125, 'a_half': 40.0, 'phi_age': 0.3, 'w_half':
-                  4.0, 'phi_weight': 0.4, 'mu': 0.4, 'gamma': 0.8, 'zeta':
-                  3.5, 'xi': 1.1, 'omega': 0.8, 'F': 50.0, 'DeltaPhiMax':
-                  10.0}
+                      4.0, 'phi_weight': 0.4, 'mu': 0.4, 'gamma': 0.8, 'zeta':
+                      3.5, 'xi': 1.1, 'omega': 0.8, 'F': 50.0, 'DeltaPhiMax':
+                      10.0}
