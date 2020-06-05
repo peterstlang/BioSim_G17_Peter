15d117f16134d26e02ed102628d191dbaecbcac2
Peter Langdalen <pelangda@nmbu.no>
2020-06-05 15:49:23 +0200

    Methods
    Made a template for how i think it will look like. All methods are subject to change

--- biosim/landscape.py
+++ biosim/landscape.py
@@ -12,5 +12,40 @@ class Cell:
     """
     cell superclass
     """
+
     def __init__(self):
         pass
+
+    def grow_fodder(self):
+        pass
+
+    def feed_animals(self):
+        pass
+
+    def procreation(self):
+        pass
+
+    def aging_animals(self):
+        pass
+
+    def animals_weight_loss(self):
+        pass
+
+    def animals_die(self):
+        pass
+
+
+class Water(Cell):
+    pass
+
+
+class Desert(Cell):
+    pass
+
+
+class Lowland(Cell):
+    pass
+
+
+class Highland(Cell):
+    pass
