5d5a9deef44d572673f79f19603d362f53340b0c
Peter Langdalen <pelangda@nmbu.no>
2020-06-07 17:28:03 +0200

    Additions landscape
    worked on some of the methods

--- biosim/landscape.py
+++ biosim/landscape.py
@@ -17,30 +17,44 @@ class Cell:
     """
     parameters = {}
 
+    @classmethod
+    def set_parameters(cls, parameters):
+        pass
+
     def __init__(self):
         self.fodder = 0
-        self.animals_in_cell = {}
-
-    @classmethod
-    def set_parameters(cls):
-        pass
+        self.herbivores = []
 
     def grow_fodder(self):
         pass
 
-    def feed_animals(self):
+    def place_animals(self, list_of_animals):
         pass
 
-    def procreation(self):
+    def feed_herbivores(self):
+        for herb in self.herbivores:
+            food_available = self.fodder
+            herb.eat(food_available)
+            self.fodder -= food_available
+
+    def feed_carnivores(self):
         pass
+
+    def procreation_herbivores(self):
+        herb_offpsring = []
+
 
     def aging_animals(self):
-        pass
+        for animals in self.herbivores:
+            animals.update_age()
 
     def animals_weight_loss(self):
         pass
 
     def animals_die(self):
+        pass
+
+    def get_num_animals(self):
         pass
 
 
