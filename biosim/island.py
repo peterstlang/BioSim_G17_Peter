4e47958ae263d266459ac1d0c69aee42dc7bfcbe
Peter Langdalen <pelangda@nmbu.no>
2020-06-11 11:20:12 +0200

    Begun sketching out the island

--- biosim/island.py
+++ biosim/island.py
@@ -6,3 +6,13 @@
 
 __author__ = 'Peter Langdalen'
 __email__ = 'pelangda@nmbu.no'
+
+class Island:
+    """
+    The island class.
+    """
+    def __init__(self, map):
+        pass
+
+    def annual_cycle(self):
+        pass
